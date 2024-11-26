9065de043a6a7dec8611dd4cd79f4d4a172528e9
Ishaan Gupta <ishaangupta02@gmail.com>
2024-11-26 14:33:27 +0530

    created 2 widgets from the flipkart website

---
+++ src/Electronics.js
@@ -0,0 +1,41 @@
+import './Electronics.css';
+
+function Electronics() {
+    return (
+        <div className='electronicsContainer'>
+            <h1>Best of Electronics</h1>
+            <div className='navContainer'>
+                <div className='navItem'>
+                    <img src='https://rukminim2.flixcart.com/image/210/210/l58iaa80/headphone/k/z/m/nord-buds-ce-oneplus-original-imagfyk4hyvgg6ze.jpeg?q=90' alt='best truewireless headphones' />
+                    <p>Best Truewireless Headphones</p>
+                </div>
+                <div className='navItem'>
+                    <img src='https://rukminim2.flixcart.com/image/210/210/xif0q/smartwatch/5/v/s/-original-imagxrhetgfuebnn.jpeg?q=90' alt='smarttrack smartwatches' />
+                    <p>Fastrack Smartwatches</p>
+                </div>
+                <div className='navItem'>
+                    <img src='https://rukminim2.flixcart.com/image/210/210/jpmxuvk0/printer-refurbished/z/r/z/u-ts207-canon-original-imafbu3xpytszpgx.jpeg?q=90' alt='printers' />
+                    <p>Printers</p>
+                </div>
+                <div className='navItem'>
+                    <img src='https://rukminim2.flixcart.com/image/210/210/kcf4lu80/speaker/mobile-tablet-speaker/h/u/f/srs-xb23-sony-original-imaftk66vjxp86h5.jpeg?q=90' alt='best selling mobile speakers' />
+                    <p>Best Selling Mobile Speakers</p>
+                </div>
+                <div className='navItem'>
+                    <img src='https://rukminim2.flixcart.com/image/210/210/kactpjk0/shaver/m/6/z/philips-s1121-45-s1121-45-original-imafry2qgxcwnm9r.jpeg?q=90' alt='best of shavers' />
+                    <p>Best of Shavers</p>
+                </div>
+                <div className='navItem'>
+                    <img src='https://rukminim2.flixcart.com/image/210/210/kl9rssw0/monitor/y/c/2/proart-display-pa278qv-27-pa278qv-asus-original-imagyfpfyzwgdygs.jpeg?q=90' alt='asus monitors' />
+                    <p>ASUS Monitors</p>
+                </div>
+                <div className='navItem'>
+                    <img src='https://rukminim2.flixcart.com/image/210/210/xif0q/monitor/e/r/g/-original-imahftaabx9tzp6q.jpeg?q=90' alt='monitors' />
+                    <p>Monitors</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Electronics;
---
+++ src/ItemsNav.js
@@ -0,0 +1,46 @@
+import './ItemsNav.css'
+
+function ItemsNav() {
+    return (
+        <div className='container'>
+            <div className='navElements'>
+                <img src='https://rukminim2.flixcart.com/flap/64/64/image/29327f40e9c4d26b.png?q=100' alt='grocery' />
+                <p>Grocery</p>
+            </div>
+            <div className='navElements'>
+                <img src='https://rukminim2.flixcart.com/flap/64/64/image/22fddf3c7da4c4f4.png?q=100' alt='mobiles' />
+                <p>Mobiles</p>
+            </div>
+            <div className='navElements'>
+                <img src='https://rukminim2.flixcart.com/fk-p-flap/64/64/image/0d75b34f7d8fbcb3.png?q=100' alt='fashion' />
+                <p>Fashion</p>
+            </div>
+            <div className='navElements'>
+                <img src='https://rukminim2.flixcart.com/flap/64/64/image/69c6589653afdb9a.png?q=100' alt='electronics' />
+                <p>Electronics</p>
+            </div>
+            <div className='navElements'>
+                <img src='https://rukminim2.flixcart.com/flap/64/64/image/ab7e2b022a4587dd.jpg?q=100' alt='home and furniture' />
+                <p>Home & Furniture</p>
+            </div>
+            <div className='navElements'>
+                <img src='https://rukminim2.flixcart.com/fk-p-flap/64/64/image/0139228b2f7eb413.jpg?q=100' alt='appliances' />
+                <p>Appliances</p>
+            </div>
+            <div className='navElements'>
+                <img src='https://rukminim2.flixcart.com/flap/64/64/image/71050627a56b4693.png?q=100' alt='flight bookings' />
+                <p>Flight Bookings</p>
+            </div>
+            <div className='navElements'>
+                <img src='https://rukminim2.flixcart.com/flap/64/64/image/dff3f7adcf3a90c6.png?q=100' alt='beauty, toys and more' />
+                <p>Beauty, Toys & More</p>
+            </div>
+            <div className='navElements'>
+                <img src='https://rukminim2.flixcart.com/fk-p-flap/64/64/image/05d708653beff580.png?q=100' alt='two wheelers' />
+                <p>Two Wheelers</p>
+            </div>
+        </div>
+    )
+}
+
+export default ItemsNav;
